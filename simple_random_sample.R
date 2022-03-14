097d75b5a5ee488bf792031e7930704e312da43e
Edgarandre5 <95248285+Edgarandre5@users.noreply.github.com>
2022-03-14 10:29:51 -0600

    Update simple_random_sample.R

--- simple_random_sample.R
+++ simple_random_sample.R
@@ -1,21 +1,14 @@
 set.seed(1111)
-setwd("/Users/edgarsanchez/Mamiferos/poligonos_a_puntos/puntos/")
+setwd("/home/")
 
-library(rgdal)
-library(raster)
-library(maptools) 
-library(rgeos)
 library(data.table)
-library(parallel)
-library(filesstrings)
-resultado <- setNames(data.table(matrix(data=character(),nrow = 10000, ncol = 6)), c("scientificName","0.083", "0.20","0.41","0.62","0.83"))
 
-archivos <- data.table(list.files(path="/Users/edgarsanchez/Mamiferos/poligonos_a_puntos/shapes/end/",pattern =".csv",full.name=F))
-a083 <- data.table(list.files(path="/Users/edgarsanchez/Mamiferos/poligonos_a_puntos/shapes/end/",pattern =".csv",full.name=T))
 
-for (j in 1:nrow(archivos)) {
-  a1 <- data.table(read.csv(paste0(a083[j]),header = T))
-  a2 <- a1[sample(nrow(a1),300),]#round(300*(1-exp(-nrow(a1)/1873.32)))
-  write.csv(a2,paste0(archivos[j]),row.names = F)
+species <- data.table(list.files(path="/home/species_presences/",pattern =".csv",full.name=F))
+species_path <- data.table(list.files(path="/home/species_presences/",pattern =".csv",full.name=T))
+
+for (j in 1:nrow(species)) {
+  a1 <- data.table(read.csv(paste0(species_path[j]),header = T))
+  write.csv(a1[sample(nrow(a1),300),] ,paste0(species[j]),row.names = F)
 }
  
